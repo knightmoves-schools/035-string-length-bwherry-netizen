db96835eb24b2d177705dbf9adae4b972ea0fe12
Brendon W <bwherry@midland.k12.ia.us>
2025-10-20 17:01:25 +0000

    gg

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-//Add code below
+
+let quoteLength = quote.length;
+let twentySeconedLetter = quote[22 - 1];
+var output = quoteLength;
+var output = twentySeconedLetter;
+
+
+document.getElementById("result").innerHTML = output;
+
+
