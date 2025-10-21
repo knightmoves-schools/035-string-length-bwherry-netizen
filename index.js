f5c27f53bec9d8bf4d363ba52f1b321d49bc47ce
Brendon W <bwherry@midland.k12.ia.us>
2025-10-21 16:57:02 +0000

    gg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let quote = "...ask not what your country can do for you — ask what you can do
 
 
 let quoteLength = quote.length;
-let twentySecondLetter = quote[22];
+let twentySecondLetter = quote[21];
 let output = `${quoteLength} [${twentySecondLetter}]`;
 
 
